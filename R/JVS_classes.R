f92175e205d2e97e4ee1fbd619a92c20096273cd
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-07-22 14:05:16 +0000

    document and translate thresholds functions and JVS_matrix related

--- R/JVS_classes.R
+++ R/JVS_classes.R
@@ -1,3 +1,56 @@
+#' @title Objets de classe JVS_matrix
+#'
+#' @description
+#' Un objet \code{JVS_matrix()} est créé d'après le modèle du plug-in
+#' d'Eurostat JVS.
+#'
+#' @param x un \code{data.frame} contenant les résultats des tests(p-values,
+#'   statistiques, etc.) et des modalitéss (Yes/No) en cas de présence d'une
+#'   variable ou d'un indicateur.
+#'
+#' @details Un objet de classe \code{\link{JVS_matrix}} est un data.frame avec
+#' 30 colonnes :
+#'
+#' * Series
+#' * Method
+#' * Period
+#' * Nobs
+#' * Start
+#' * End
+#' * Adjustment
+#' * Presence of Seasonality in the Raw Series
+#' * Presence of TD effects
+#' * Log-Transformation
+#' * ARIMA Model
+#' * LeapYear
+#' * MovingHoliday
+#' * NbTD
+#' * Noutliers
+#' * Outlier1
+#' * Outlier2
+#' * Outlier3
+#' * Residual Seasonality in SA Series (F-test)
+#' * Residual TD Effect
+#' * Q-Stat (for X13)
+#' * Final Henderson Filter
+#' * Stage 2 Henderson Filter
+#' * Seasonal Filter
+#' * Quality
+#' * Autocorrelation of order 1 of the SA series
+#' * Ljung-Box Test (P-value)
+#' * Autocorrelation negative and significant
+#' * Irregular Standard-Deviation
+#' * Max-Adj
+#'
+#' @returns
+#' \code{JVS_matrix()} renvoie un objet de classe \code{\link{JVS_matrix}}.
+#'
+#' @encoding UTF-8
+#' @keywords internal
+#' @name fr-JVS_matrix
+NULL
+#> NULL
+
 #' @title JVS matrix object
 #'
 #' @description
@@ -7,7 +60,7 @@
 #' @param x a \code{data.frame} containing the output variables' values
 #' (test p-values, test statistics, etc.) and modalities (Yes/No).
 #'
-#' @details A\code{\link{QR_matrix}} object is a data.frame with 30 items:
+#' @details A\code{\link{JVS_matrix}} object is a data.frame with 30 items:
 #'
 #' * Series
 #' * Method
@@ -46,6 +99,7 @@
 #' @encoding UTF-8
 #' @name JVS_matrix
 #' @export
+#' @seealso [Traduction française][fr-JVS_matrix()]
 JVS_matrix <- function(x = list()) {
     UseMethod("JVS_matrix", x)
 }
